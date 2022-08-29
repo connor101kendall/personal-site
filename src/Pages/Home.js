5ee5d772320d49a417db78b857d1317c26791037
connor <connor101kendall@gmail.com>
2022-08-29 11:52:41 -0400

    styling fix

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,16 +7,17 @@ export default function Home() {
           <img className="Image" alt="Portrait" src="img/Portrait.png"></img>
           <div>
           <h3>I'M CONNOR KENDALL</h3>
+          <br />
           <h4>A Biomedical Engineering student at the University Of Waterloo</h4>
           This is a personal site I created to showcase some of my skills! 
-          <br></br>
-          <br></br>
+          <br />
+          <br />
           Feel free to have a look around and 
           check out my past experiences with the NavBar at the top. You will also find my socials at the bottom.
           Don't hesitate to send me an&nbsp;
           <a className='Link' href="mailto:connor101kendall@gmail.com?subject=Website Contact">email</a> 
           &nbsp;to discuss any opportunities!
-          <br></br>
+          <br />
           
           <a href="img/Connor_Kendall_Resume.pdf" download="Connor_Kendall_Resume.pdf">
          <button className='Button' type="button">DOWNLOAD MY RESUME</button>
