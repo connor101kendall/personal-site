d8dca4434fd9be693d26c99bca4bd20d48325668
connor <connor101kendall@gmail.com>
2022-08-31 13:58:07 -0400

    Dates added

--- src/Pages/Experience.js
+++ src/Pages/Experience.js
@@ -24,8 +24,8 @@ export default function Experience() {
                 {data.map((item, i) => (
                     <div className='item'>
                         <div className='title' onClick={() => toggle(i)}>
-                            <h3>{item.title}</h3>
-                            <FontAwesomeIcon className="Icon" icon={selected === i ? faAngleUp : faAngleDown} />
+                            <h3>{item.title}<FontAwesomeIcon className="Icon2" icon={selected === i ? faAngleUp : faAngleDown} /></h3>
+                            <h4 className='date'>{item.date}</h4>
                         </div>
                         <p className='loc'>{item.loc}</p>
                         <div className={selected === i ? 'content show' : 'content'}>
@@ -45,30 +45,35 @@ const data = [
     {
         title: 'Front-end Developer',
         loc: 'WeAccelerate/eButterfly | Remote',
+        date: 'June 2022 - August 2022',
         d1: 'Developed UI/UX using React.js on eBLabs project',
         d2: 'Collaborated with a remote dev team using GitHub, Slack, and Asana',
     },
     {
         title: 'Pump Operator',
         loc: 'Blue Beacon | Milton, ON',
+        date: 'May 2022 - August 2022',
         d1: 'Operated high pressure pumps using a variety of chemicals to clean large trucks',
         d2: 'Collaborated consistently with a team to achieve quotas in a high intensity workplace',
     },
     {
         title: 'Operations Associate',
         loc: 'Well.ca | Guelph, ON',
+        date: 'June 2021 - August 2021',
         d1: 'Utilized hand-held devices and computers to monitor inventory levels and completed audits',
         d2: 'Rapidly learned new skills daily to increase efficiency and optimize material delivery processes',
     },
     {
         title: 'Line Preparation Cook',
         loc: 'ClubLink | Acton, ON',
+        date: 'June 2019 - October 2020',
         d1: 'Set up and prepared cooking supplies and workstations to maximize productivity',
         d2: 'Collaborated with servers and customers in a high stress environment to produce dishes with consistent attention to detail and quality',
     },
     {
         title: 'Camp Counseller',
         loc: 'Ways of the Woods | Guelph, ON',
+        date: 'July 2018 – August 2018',
         d1: 'Mentored children aged 8-14 through the basic construction of a birdhouse using handheld tools',
         d2: 'Collaborated with senior camp counselors to organize events and resolve conflicts between campers',
     },
